a1a2824f658a250bec939c7320d9ab38d198f0b0
Dmitry Mozzherin <dmozzherin@gmail.com>
2015-04-20 22:58:31 -0400

    add base class

--- lib/taxamatch_rb.rb
+++ lib/taxamatch_rb.rb
@@ -1,5 +1,16 @@
 require "taxamatch_rb/version"
+require "damerau-levenshtein"
+require "taxamatch_rb/base"
+require "taxamatch_rb/atomizer"
+require "taxamatch_rb/normalizer"
+require "taxamatch_rb/phonetizer"
+require "taxamatch_rb/authmatch"
 
-module TaxamatchRb
-  # Your code goes here...
+if RUBY_VERSION < "1.9.1"
+  fail "IMPORTANT: Parsley-store gem  requires ruby >= 1.9.1"
+end
+
+# encoding: utf-8
+# Taxamatch is a namespace module
+module Taxamatch
 end
